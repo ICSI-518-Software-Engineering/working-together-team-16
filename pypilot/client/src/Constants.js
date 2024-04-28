75fe662107013c5549fada2c318e4ca8ab69c4dd
Badrinath SE <157666783+BadrinathSE@users.noreply.github.com>
2024-04-28 11:45:57 -0400

    Update Constants.js

--- pypilot/client/src/Constants.js
+++ pypilot/client/src/Constants.js
@@ -1,3 +1,3 @@
 export const BACKEND_URL = "http://localhost:9001/"
-export const API_KEY = process.env.REACT_APP_API_KEY
+export const API_KEY = "sk-proj-LaFnLSJXNxjGrYYEAjSjT3BlbkFJWSpbTWNeXrvHK7pwiKlS"
 export const OPENAI_BASE_URL = "https://api.openai.com/v1/chat/completions"
